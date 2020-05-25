13b6d6074fbec31086a7748f42a269f274cde50c
MizyurkinaOlga <olga.mizyurkina@mail.ru>
2020-05-25 19:35:09 +0700

    open file for processing

--- WordProcessing/Form1.Designer.cs
+++ WordProcessing/Form1.Designer.cs
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pathSourceFile = new System.Windows.Forms.TextBox();
+            this.buttonBrowseFile = new System.Windows.Forms.Button();
+            this.labelFilePreparation = new System.Windows.Forms.Label();
+            this.labelDownloaded = new System.Windows.Forms.Label();
+            this.buttonReadWord = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // pathSourceFile
+            // 
+            resources.ApplyResources(this.pathSourceFile, "pathSourceFile");
+            this.pathSourceFile.Name = "pathSourceFile";
+            // 
+            // buttonBrowseFile
+            // 
+            resources.ApplyResources(this.buttonBrowseFile, "buttonBrowseFile");
+            this.buttonBrowseFile.Name = "buttonBrowseFile";
+            this.buttonBrowseFile.UseVisualStyleBackColor = true;
+            this.buttonBrowseFile.Click += new System.EventHandler(this.browseFile_Click);
+            // 
+            // labelFilePreparation
+            // 
+            resources.ApplyResources(this.labelFilePreparation, "labelFilePreparation");
+            this.labelFilePreparation.Name = "labelFilePreparation";
+            // 
+            // labelDownloaded
+            // 
+            resources.ApplyResources(this.labelDownloaded, "labelDownloaded");
+            this.labelDownloaded.ForeColor = System.Drawing.Color.Green;
+            this.labelDownloaded.Name = "labelDownloaded";
+            // 
+            // buttonReadWord
+            // 
+            resources.ApplyResources(this.buttonReadWord, "buttonReadWord");
+            this.buttonReadWord.Name = "buttonReadWord";
+            this.buttonReadWord.UseVisualStyleBackColor = true;
+            this.buttonReadWord.Click += new System.EventHandler(this.buttonReadWord_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonReadWord);
+            this.Controls.Add(this.labelDownloaded);
+            this.Controls.Add(this.labelFilePreparation);
+            this.Controls.Add(this.buttonBrowseFile);
+            this.Controls.Add(this.pathSourceFile);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pathSourceFile;
+        private System.Windows.Forms.Button buttonBrowseFile;
+        private System.Windows.Forms.Label labelFilePreparation;
+        private System.Windows.Forms.Label labelDownloaded;
+        private System.Windows.Forms.Button buttonReadWord;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WordProcessing/Form1.cs
+++ WordProcessing/Form1.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WordProcessing
 {
     public partial class Form1 : Form
     {
+        OpenFileDialog ofd = new OpenFileDialog();
+        Stream fileStream;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void browseFile_Click(object sender, EventArgs e)
+        {
+            labelFilePreparation.Visible = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                pathSourceFile.Text = ofd.FileName;
+                fileStream = ofd.OpenFile();
+                labelFilePreparation.Visible = false;
+                labelDownloaded.Visible = true;
+            }
+
+        }
+
+        private void buttonReadWord_Click(object sender, EventArgs e)
+        {
+            byte[] tmp = new byte[30];
+            fileStream.Read(tmp,0,20);
+            label2.Text = System.Text.Encoding.Default.GetString(tmp);
+            label2.Visible = true;
         }
     }
 }
